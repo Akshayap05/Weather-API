e1d4dd54cf0d4ada01b556c7690252a77e95d3f9
Akshaya <Akshayap0504@gmail.com>
2024-03-26 01:15:01 +0000

    Update notes.py

--- notes.py
+++ notes.py
@@ -159,20 +159,19 @@ def get_pollutant_data_for_all_cities():
 # Fetch the pollutant data for all cities
 pollutant_data_all_cities = get_pollutant_data_for_all_cities()
 
-if not get_pollutant_data_for_all_cities.empty:
-    fig, ax = plt.subplots(figsize=(10, 6))
-    pollutant_data_all_cities.plot(kind='bar', x='location', ax=ax)
-    plt.xlabel('City')
-    plt.ylabel('Average Concentration')
-    plt.title('Pollutant Comparison for All Cities')
-    plt.xticks(rotation=45)
-    plt.legend(loc='upper right')
-    plt.tight_layout()
 
-    # Display the plot
-    st.pyplot(fig)
-else:
-    st.error('Error getting data')
+# Plot the pollutants for all cities
+fig, ax = plt.subplots(figsize=(10, 6))
+pollutant_data_all_cities.plot(kind='bar', x='location', ax=ax)
+plt.xlabel('City')
+plt.ylabel('Average Concentration')
+plt.title('Pollutant Comparison for All Cities')
+plt.xticks(rotation=45)
+plt.legend(loc='upper right')
+plt.tight_layout()
+
+# Display the plot
+st.pyplot(fig)
 
 
 
