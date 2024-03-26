d10f0e5f5781cc9f5a10da72b5180461385f6e92
Akshaya <Akshayap0504@gmail.com>
2024-03-26 15:20:39 +0000

    Update notes.py

--- notes.py
+++ notes.py
@@ -37,9 +37,8 @@ def get_details(cities):
     
 cities = ['London', 'Manchester', 'Birmingham', 'Glasgow', 'Leeds', 'Liverpool', 'Sheffield', 'Bristol', 'Edinburgh', 'Leicester',  'York', 'Cardiff', 'Brighton', 'Coventry', 'Bath']
 
-with st.columns(3)[1]:
-    st.sidebar.image('logo.png', width = 100, use_column_width=False, output_format='PNG')
 
+st.sidebar.image('logo.png', width = 100, use_column_width=False, align='center')
 selected_city = st.sidebar.selectbox('Choose a city', cities)
 
 
