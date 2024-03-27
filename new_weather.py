fb7cd82bd7c2b568f4459216803058de5275fbaa
Akshaya <Akshayap0504@gmail.com>
2024-03-27 02:18:40 +0000

    Update new_weather.py

--- new_weather.py
+++ new_weather.py
@@ -95,7 +95,112 @@ if tab == 'Air Quality':
         st.markdown(f"<div style='padding: 10% 3% 3% 3%; background-color: #0074cc;'><h3 style='text-align: center;'>{Ozone}</h3>", unsafe_allow_html=True)
 
 
+# Credentials to connect to database in pagila postgresql:
+        
+db_user = st.secrets["DB_USER"]
+db_password = st.secrets["DB_PASSWORD"]
+db_host = st.secrets["DB_HOSTS"]
+db_name = st.secrets["DB_NAME"]
+db_port = st.secrets["DB_PORT"]
 
+# Connect to database, get weather details through table using query:
+
+
+def get_data(selected_city):
+
+    try:
+
+        engine = create_engine(f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}')
+        query1 = f"""
+                SELECT DISTINCT to_char(date, 'YYYY-MM-DD') AS date, location, temperature, co, no2, o3
+                FROM student.weather
+                WHERE location='{selected_city}'
+                ORDER BY date ASC"""
+        data = pd.read_sql(query1, engine)
+
+        query2 = f"""
+                SELECT DISTINCT to_char(date, 'YYYY-MM-DD') AS date, location, AVG(co) AS avg_co, AVG(no2) AS avg_no2, AVG(o3) AS avg_o3
+                FROM student.weather
+                WHERE location = '{selected_city}'
+                GROUP BY date, location
+                ORDER BY date ASC""" 
+        air_quality = pd.read_sql(query2, engine)
+
+        return data,air_quality
+    except Exception as e:
+        st.error(f'Error: {e}')
+
+
+
+# Query to get air quality (pollution data):
+   
+def get_pollution_data_for_all_cities():
+
+    try:
+        engine = create_engine(f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}')
+        query3 = f"""
+                SELECT location, AVG(co) AS avg_co, AVG(no2) AS avg_no2, AVG(o3) AS avg_o3
+                FROM student.weather
+                GROUP BY location
+                """
+        pollution_data_cities = pd.read_sql(query3, engine)
+        return pollution_data_cities
+    except Exception as e:
+        st.error(f'Error: {e}')
+
+
+
+# Graphs:
+
+def main():
+    data, air_quality = get_data(selected_city)
+    pollution_data_cities = get_pollution_data_for_all_cities()
+    if tab =='Weather':
+    # Calculating average temperature and plot the line chart using Matplotlib
+        daily_average_temp = data.groupby('date')['temperature'].mean()
+
+        plt.figure(figsize=(10, 6))
+        plt.plot(daily_average_temp.index, daily_average_temp.values, marker='o', linestyle='-')
+        plt.xlabel('Date')
+        plt.ylabel('Average Temperature (°C)')
+        plt.title(f'Average Temperature in {selected_city} over time')
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+
+        st.pyplot(plt)
+    # Plot the air quality data (pollution) for all cities to compare with eachother
+
+    # Plot the air quality data (pollution) for city selected:
+    elif tab =='Air Quality':
+        fig, ax = plt.subplots(figsize=(8, 6))
+        air_quality.set_index('date').plot(kind='bar', ax=ax)
+        plt.xlabel('Date')
+        plt.ylabel('Average Concentration')
+        plt.title(f'Change in Pollution levels over time in {selected_city}')
+        plt.xticks(rotation=0)
+        plt.legend(loc='upper right')
+        plt.tight_layout()
+
+        st.pyplot(fig)
+
+        if st.button('See all cities'):
+        
+            fig, ax = plt.subplots(figsize=(10, 6))
+            pollution_data_cities.plot(kind='bar', x='location', ax=ax)
+            plt.xlabel('City')
+            plt.ylabel('Average Concentration')
+            plt.title('Pollutant Comparison for All Cities')
+            plt.xticks(rotation=45)
+            plt.legend(loc='upper right')
+            plt.tight_layout()
+
+            st.pyplot(fig) 
+    else:
+        st.error("Failed to get data.")
+
+#data, air_quality, air_quality_all_cities = get_data(selected_city)
+
+main()
 
 
 
