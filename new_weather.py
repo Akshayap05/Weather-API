7c8f339df3a67ffd081494e70c9078f80569a486
Akshaya <Akshayap0504@gmail.com>
2024-03-27 01:45:29 +0000

    Update new_weather.py

--- new_weather.py
+++ new_weather.py
@@ -81,16 +81,16 @@ if tab == 'Air Quality':
     b1, b2, b3, b4 = st.columns(4)
     with b1:
         st.markdown("<h5 style='text-align: center;'>UV Index</h5>", unsafe_allow_html=True)
-        st.markdown(f"<h2 style='text-align: center;'>{UV_index}</h2>", unsafe_allow_html=True)
+        st.markdown(f"<h3 style='text-align: center;'>{UV_index}</h3>", unsafe_allow_html=True)
     with b2:
         st.markdown("<h5 style='text-align: center;' >CO</h5>", unsafe_allow_html=True)
-        st.markdown(f"<h2 style='text-align: center;'>{CO}</h2>", unsafe_allow_html=True)
+        st.markdown(f"<h3 style='text-align: center;'>{CO}</h3>", unsafe_allow_html=True)
     with b3:
         st.markdown("<h5 style='text-align: center;'>NO2</h5>", unsafe_allow_html=True)
-        st.markdown(f"<h2 style='text-align: center;'>{NO2}</h2>", unsafe_allow_html=True)
+        st.markdown(f"<h3 style='text-align: center;'>{NO2}</h3>", unsafe_allow_html=True)
     with b4:
         st.markdown("<h5 style='text-align: center;'>Ozone (O3)</h5>", unsafe_allow_html=True)
-        st.markdown(f"<h2 style='text-align: center;'>{Ozone}</h2>", unsafe_allow_html=True)
+        st.markdown(f"<h3 style='text-align: center;'>{Ozone}</h3>", unsafe_allow_html=True)
 
 
 
