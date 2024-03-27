6c4fa19f6d49f72f9d0ec15d76bca252422c9465
Akshaya <Akshayap0504@gmail.com>
2024-03-27 17:03:19 +0000

    Update new_weather.py

--- new_weather.py
+++ new_weather.py
@@ -58,8 +58,8 @@ if tab == 'Weather':
     #temperature, humidity,condition = get_details(selected_city)
     a1, a2, a3 = st.columns(3)
     with a1:
-        st.markdown("<div style='padding: 8% 3% 5% 9%;  background-color: #263c52;'><h6 style='text-align: center;'>Temperature:</h6></div>", unsafe_allow_html=True)
-        st.markdown(f"<div style='padding: 8% 3% 5% 9%; margin-bottom: 14px; background-color: #263c52;'><h3 style='text-align: center;'>{temperature}°C</h3></div>", unsafe_allow_html=True)
+        st.markdown("<div style='padding: 6% 3% 3% 6%;  background-color: #263c52;'><h6 style='text-align: center;'>Temperature:</h6></div>", unsafe_allow_html=True)
+        st.markdown(f"<div style='padding: 6% 3% 3% 6%; margin-bottom: 14px; background-color: #263c52;'><h3 style='text-align: center;'>{temperature}°C</h3></div>", unsafe_allow_html=True)
 
     with a2:
         st.markdown("<div style='padding: 8% 3% 5% 9%; background-color: #263c52;'><h6 style='text-align: center;'>Humidity:</h6></div>", unsafe_allow_html=True)
