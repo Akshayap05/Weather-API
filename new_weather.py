e247e0713bd96394c276e6a345e1c031542ebd56
Akshaya <Akshayap0504@gmail.com>
2024-03-27 10:01:39 +0000

    Update new_weather.py

--- new_weather.py
+++ new_weather.py
@@ -13,8 +13,8 @@ st.title(f'🌦️UK Weather Explorer')
 #st.write("**Select a city from the dropdown box to explore its weather.**")
 st.markdown("<h6 style='text-align: left;'>First select a city to explore its weather</h6>", unsafe_allow_html=True)
 
-with open('style.css') as f:
-    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+#with open('style.css') as f:
+#    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 
 # Get weather data from API for different cities:
